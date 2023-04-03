2828350f521ee99ecd21af0ffc7c3e754032a22e
Claudio Moscoso <cmoscosoz@gmail.com>
2023-04-03 21:07:10 +1200

    Wrapping app with context provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
-import '../styles/app.scss'
-import type { AppProps } from 'next/app'
+import "../styles/app.scss";
+import type { AppProps } from "next/app";
+
+//context provider
+import { AppWrapper } from "context/NotificationsContext";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <AppWrapper>
+      <Component {...pageProps} />
+    </AppWrapper>
+  );
 }
