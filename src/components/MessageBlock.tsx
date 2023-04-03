b6692c363edf5179cef3b0eadab4ed52e0f78d08
Claudio Moscoso <cmoscosoz@gmail.com>
2023-04-03 22:39:02 +1200

    Fix bug when there is not item selected

--- src/components/MessageBlock.tsx
+++ src/components/MessageBlock.tsx
@@ -1,17 +1,21 @@
-import { useAppContext } from "context/NotificationsContext";
 import React from "react";
+import { useAppContext } from "context/NotificationsContext";
 
 const MessageBlock: React.FC = () => {
-  
   const state = useAppContext();
-  const { selectedNotification } = state
-  const className = selectedNotification ? "opacity-100" : "opacity-0"
+  const { selectedNotification } = state;
   return (
-    <div className={`bg-gray-200 w-full rounded-lg p-2 h-96 ${className}`}>
-      <h4 className={"text-center py-2"}>{selectedNotification.title}</h4>
-      <div className={"h-[1px] w-full bg-black/[0.1]"} />
-      <p className={"px-2 whitespace-pre-wrap text-justify py-4"}>{selectedNotification.message}</p>
-    </div>
+    <React.Fragment>
+      {selectedNotification && (
+        <div className={`bg-gray-200 w-full rounded-lg p-2 h-96`}>
+          <h4 className={"text-center py-2"}>{selectedNotification.title}</h4>
+          <div className={"h-[1px] w-full bg-black/[0.1]"} />
+          <p className={"px-2 whitespace-pre-wrap text-justify py-4"}>
+            {selectedNotification.message}
+          </p>
+        </div>
+      )}
+    </React.Fragment>
   );
 };
 
