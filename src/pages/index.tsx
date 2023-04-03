dd185532d757d18baa5a0393c3fabfd2479029ff
Claudio Moscoso <cmoscosoz@gmail.com>
2023-04-03 22:39:32 +1200

    clean up

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,46 +1,11 @@
-import React, { useEffect, useRef, useState } from "react";
-import gsap from "gsap";
-import { FiMail } from "react-icons/fi";
-import data from "data/notifications.json";
+import React from "react";
 
 //components imports
 import MessageList from "components/MessageList";
 import MessageBlock from "components/MessageBlock";
+import NotificationHeader from "components/NotificationHeader";
 
 const Home: React.FC = () => {
-  const { notifications } = data;
-  const notificationRef = useRef<HTMLDivElement>(null);
-  const notificationIconRef = useRef<HTMLDivElement>(null);
-  const contentRef = useRef<HTMLDivElement>(null);
-  const [isOpen, setIsOpen] = useState<boolean>(false);
-  const [notificationCount, setNotificationCount] = useState<number>(
-    notifications.length
-  );
-
-  useEffect(() => {
-    const notication = notificationRef.current;
-    const noticationIcon = notificationIconRef.current;
-
-    gsap.fromTo(
-      noticationIcon,
-      {
-        opacity: 0,
-        scale: 1.3,
-      },
-      {
-        delay: 3,
-        opacity: 1,
-        scale: 1,
-        ease: "Power.out3",
-      }
-    );
-
-    return () => {
-      gsap.killTweensOf(notication);
-    };
-  }, [notificationRef, notificationIconRef, contentRef]);
-
-  console.log(notifications);
 
   return (
     <div
@@ -49,23 +14,7 @@ const Home: React.FC = () => {
       <div
         className={"w-full h-fit flex justify-center items-center px-20 py-10"}
       >
-        <div
-          ref={notificationRef}
-          className={
-            "min-w-fit w-full bg-gray-200 rounded-full px-4 py-2 flex cursor-pointer justify-center items-center space-x-2 hover:shadow-md relative text-2xl"
-          }
-          onClick={() => setIsOpen(!isOpen)}
-        >
-          <div className={"inline-flex relative items-center"}>
-            <p>Notification</p>
-            <div ref={notificationIconRef} className={"absolute -right-10"}>
-              <FiMail />
-              <span className={"absolute -top-1.5 text-xs -right-2"}>
-                {notificationCount}
-              </span>
-            </div>
-          </div>
-        </div>
+        <NotificationHeader />
       </div>
       <div
         className={
