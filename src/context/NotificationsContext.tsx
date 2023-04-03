5c63b9eb80e094e340eb6d60a8ba6eec4f7178fc
Claudio Moscoso <cmoscosoz@gmail.com>
2023-04-03 21:54:52 +1200

    Add notifications states

--- src/context/NotificationsContext.tsx
+++ src/context/NotificationsContext.tsx
@@ -5,8 +5,12 @@ const NotificationContext = React.createContext({})
 
 const AppWrapper = ({children}:{children:React.ReactNode}) => {
     const notificationsState = data.notifications.map((notification) => ({...notification, unread: true}))
+    const [notifications, setNotifications] = React.useState<Notification[]>(notificationsState)
+    const [selectedNotification, setSelectedNotification] = React.useState<Notification | null>(null)
+    
+    const NotificationState = { notifications,setNotifications, selectedNotification, setSelectedNotification }
     return(
-        <NotificationContext.Provider value={notificationsState}>{children}</NotificationContext.Provider>
+        <NotificationContext.Provider value={NotificationState}>{children}</NotificationContext.Provider>
     )
 }
 
