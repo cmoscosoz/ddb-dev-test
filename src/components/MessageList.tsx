d9d7771f7a1ae2859d043f887e6b313f754ba861
Claudio Moscoso <cmoscosoz@gmail.com>
2023-04-03 21:27:57 +1200

    Add unread message and onclick handle

--- src/components/MessageList.tsx
+++ src/components/MessageList.tsx
@@ -1,18 +1,35 @@
-import data from "data/notifications.json";
+import { useAppContext } from "context/NotificationsContext";
+import { NotificationType } from "types/notification";
 
 const MessageList: React.FC = () => {
-    console.log(data)
-    return(
-        <div className={"bg-gray-200 w-full h-full rounded-lg p-2 h-60"}>
-          <p className="text-center tracking-wider">LIST OF MESSAGES</p>
-          <ul>
-            {data.notifications.map((notification) => (
-                <li className="text-slate-600 px-2 py-1 hover:text-slate-900 hover:bg-gradient-to-r from-yellow-400 via-yellow-300 to-yellow-200 rounded-lg cursor-pointer" key={notification.id}>{notification.title}</li>
-            ))}
-           
-          </ul>
-        </div>
-    )
-}
+  const notifications = useAppContext();
+  const handleItemClick = (id: number) => {
+    console.log(id);
+  }
+  return (
+    <div className={"bg-gray-200 w-full h-full rounded-lg p-2 h-60"}>
+      <p className="text-center tracking-wider">LIST OF MESSAGES</p>
+      <ul>
+        {notifications.map((notification: NotificationType) => {
+          const className = notification.unread ? "font-bold" : "";
+          return (
+            <li
+            onClick={() => handleItemClick(notification.id)}
+              className={`${className} text-slate-600 px-2 py-1 hover:text-slate-900 hover:bg-gradient-to-r from-yellow-400 via-yellow-300 to-yellow-200 rounded-lg cursor-pointer flex justify-between items-center`}
+              key={notification.id}
+            >
+              <span>{notification.title}</span>
+              {notification.unread && (
+                <span className="px-2 font-light text-xs text-red-400">
+                  unread
+                </span>
+              )}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
 
-export default MessageList
+export default MessageList;
