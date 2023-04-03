ac8270f38b9cf9c619282adffd9fa20c987f1f35
Claudio Moscoso <cmoscosoz@gmail.com>
2023-04-03 20:52:26 +1200

    Add list items styles

--- src/components/MessageList.tsx
+++ src/components/MessageList.tsx
@@ -4,10 +4,10 @@ const MessageList: React.FC = () => {
     console.log(data)
     return(
         <div className={"bg-gray-200 w-full h-full rounded-lg p-2 h-60"}>
-          <p>LIST OF MESSAGES</p>
+          <p className="text-center tracking-wider">LIST OF MESSAGES</p>
           <ul>
             {data.notifications.map((notification) => (
-                <li key={notification.id}>{notification.title}</li>
+                <li className="text-slate-600 px-2 py-1 hover:text-slate-900 hover:bg-gradient-to-r from-yellow-400 via-yellow-300 to-yellow-200 rounded-lg cursor-pointer" key={notification.id}>{notification.title}</li>
             ))}
            
           </ul>
